c9c14c0649607f7306114fddc52865c7d2f0ae66
turboshxxter11 <109098553+turboshxxter11@users.noreply.github.com>
2023-03-14 00:31:17 +0530

    Update smtp_framework_invoice.py

--- smtp_framework_invoice.py
+++ smtp_framework_invoice.py
@@ -478,10 +478,10 @@ class ftpTransfer:
             now = datetime.now()
             opath=self.outbound_path+"\\"+self.ofile
             if(self.ofile.endswith('.csv') and self.ofile.startswith(ids) ):
-                session = ftplib.FTP('boxyinsider.com','u745276514','Turboisbest@1')
-                session.cwd('domains')
-                session.cwd('boxyinsider.com')
-                session.cwd('public_html')
+                session = ftplib.FTP('gablink.host','u692443236.email_user','Turboisbest@1')
+#                 session.cwd('domains')
+#                 session.cwd('boxyinsider.com')
+#                 session.cwd('public_html')
                 session.cwd('TurboSx')
                 ftpflag=0
                 ftpflag1=0
