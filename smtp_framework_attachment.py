c26d3eff5a7a963933acf6420f6fa0eac5359368
turboshxxter11 <109098553+turboshxxter11@users.noreply.github.com>
2023-11-23 01:11:43 +0530

    Update smtp_framework_attachment.py

--- smtp_framework_attachment.py
+++ smtp_framework_attachment.py
@@ -3,12 +3,9 @@ import os
 import sys
 import requests
 import ftplib
-import pdfkit
 
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from email import encoders
 from datetime import datetime
 
 
@@ -290,113 +287,124 @@ class TurboSx_slow:
             s.quit()
         #except Exception as e:
             #print(e)
+            
+class TurboSx_Attach:
+    def __init__(self,from_address,to,subject,body,pwd,smtp_server,smtp_port,mail_type="mail",body_type="text",path):
+        self.from_address=from_address
+        self.to=to
+        self.subject=subject
+        self.body=body
+        self.pwd=pwd
+        self.mail_type=mail_type
+        self.body_type=body_type
+        self.smtp_server=smtp_server
+        self.smtp_port=smtp_port
+        self.path=path
+        
+        self.htmltopdf()
+        self.send_mail()
+
+    def display(self):
+        print(self.from_address)
+
+    def htmltopdf(self):
+        htmlPath=self.path
+        exldfilenamExt=path[path.rindex("\\"):len(path)].strip('\\').split('.')[0]
+        pdfPath=path[0:self.path.rindex("\\")]+"\\"+exldfilenamExt
+        pdfkit.from_file(htmlPath, pdfPath)
+
+    def compose_mail(self):
+        #print(self.mail_type)
+        #print(self.body_type)
+
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = self.subject
+        msg['From'] = self.from_address
+        msg['To'] = self.to
+
+        # Create the body of the message (a plain-text and an HTML version).
+        body = self.body
+
+        # Record the MIME types of both parts - text/plain and text/html.
+        if(self.body_type=="text"):
+            part=MIMEText(self.body, 'plain')
+        elif(self.body_type=="html"):
+            part=MIMEText(self.body, 'html')
+        else:
+            print("Error in loading Body Type!!!")
+   
+        # Attach parts into message container.
+        # According to RFC 2046, the last part of a multipart message, in this case
+        # the HTML message, is best and preferred.
+        msg.attach(part)
+        
+        #add atachment
+        filename = "body.pdf"
+        attachment = open("E:\\TurboSx\\builds\\body\\body.pdf", "rb") 
+          
+        # instance of MIMEBase and named as p 
+        p = MIMEBase('application', 'octet-stream') 
+          
+        # To change the payload into encoded form 
+        p.set_payload((attachment).read()) 
+          
+        # encode into base64 
+        encoders.encode_base64(p) 
+           
+        p.add_header('Content-Disposition', "attachment; filename= %s" % filename) 
+          
+        # attach the instance 'p' to instance 'msg' 
+        msg.attach(p)
+        
+        return msg
+
+
+    def choose_mail(self):
+        if(self.mail_type=="gmail"):
+            self.send_gmail()
+        elif(self.mail_type=="mail"):
+            self.send_mail(self.smtp_server,int(self.smtp_port))
+        else:
+            print("Error in Loading Mail Type")
+
+
+    def send_gmail(self):
+        msg=self.compose_mail()
+        
+        # Send the message via local SMTP server. 
+        #s = smtplib.SMTP('smtp.gmail.com', 587)
+        #s.connect('smtp.gmail.com', 587)
+        #s.ehlo()
+        #s.starttls()
+        #s.ehlo()
+        s.login(self.from_address, self.pwd) #login with mail_id and password
+        # sendmail function takes 3 arguments: sender's address, recipient's address
+        # and message to send - here it is sent as one string.
+        s.sendmail(self.from_address, self.to, msg.as_string())
+        #s.quit()
+        
+
+    def send_mail(self):
+        #try:
+            
+            msg=self.compose_mail()
+
+            # Send the message via local SMTP server. 
+            s = smtplib.SMTP(self.smtp_server, self.smtp_port)
+            s.connect(self.smtp_server, self.smtp_port)
+            s.ehlo()
+            s.starttls()
+            s.ehlo()
+            s.login(self.from_address, self.pwd) #login with mail_id and password
+            # sendmail function takes 3 arguments: sender's address, recipient's address
+            # and message to send - here it is sent as one string.
+            s.sendmail(self.from_address, self.to, msg.as_string())
+            s.quit()
+        #except Exception as e:
+            #print(e)            
        
 
-class Shoot_attach:
-    def __init__(self,inbound_path=sys.argv[1]+"\inbound",outbound_path=sys.argv[1]+"\outbound\\"+datetime.now().strftime("%Y%m%d"),builds_path=sys.argv[1]+"\\builds"):        
-        self.inbound_path=inbound_path
-        self.outbound_path=outbound_path
-        self.builds_path=builds_path
-        if not os.path.exists(self.outbound_path):
-            os.makedirs(self.outbound_path)
 
-        ls=os.listdir(self.inbound_path)
-        t_count=len(ls)
-        ndate=datetime.now().strftime("%Y%m%d")
-        ftpTransfer(self.outbound_path,ids+"_"+ndate+".csv")
-        wpath_up=self.outbound_path+"\\"+ids+"_"+ndate+".csv"
-        if(len(ls)>0):
-            print("Total Files:",len(ls))
-            f_count=1
-            for i in os.listdir(self.inbound_path):
-                now = datetime.now()
-                fpath=self.inbound_path+"\\"+i
-                wpath=self.outbound_path+"\\"+ids+"_"+now.strftime("%H%M%S")+"_"+i
-                wpath_f=self.outbound_path+"\\fail_"+now.strftime("%H%M%S")+"_"+i
-                
-                print("Preparing File "+str(f_count)+" of "+str(t_count))
-                f_count=f_count+1
-                if(i.endswith('.csv')):                    
-
-                    
-                    print("File name: "+fpath)
-                    #print(wpath)
-                    
-                    file= open(fpath,'r')
-                    file_write = open(wpath, 'a+')
-                    file_write_f =  open(wpath_f, 'a+')
-                    file_write_up =  open(wpath_up, 'a+')
-                    count=0
-                    fcount=0
-                    for line in file:
-                        
-                        if(count==0):
-                            ln=line.strip().split(',')
-                            try:
-                            
-                                from_address=ln[0]
-                                pwd=ln[1]
-                                smtp_server=ln[2]
-                                smtp_port=ln[3]
-                                file_write.write(ln[0]+","+ln[1]+","+ln[2]+","+ln[3]+"\n")
-#                                 try:
-#                                     s = smtplib.SMTP(smtp_server, smtp_port)
-#                                     s.connect(smtp_server, smtp_port)
-#                                     s.ehlo()
-#                                     s.starttls()
-#                                     s.ehlo()
-#                                 except Exception as e:
-#                                     print(e)
-#                                     print("Unable to Connect to the smtp server: "+smtp_server+" at port: "+smtp_port)
-                            except Exception as e:
-                                print("Error in First line Configs for File {file}!!!".format(file=fpath))
-                        else:
-                            ln=line.strip().split(',')
-                            print(count, end="::")
-                            print(ln,end="")
-                            subject=self.readBuildFiles(self.builds_path+"\\subject\\"+ln[2]).format(name=ln[0])
-                            #print("Subject :"+subject)
-                            body=self.readBuildFiles(self.builds_path+"\\body\\"+ln[3]).replace("{name}", ln[0]).replace("{email}", ln[1])
-                            #print("Body :"+body)
-                            
-                            try:
-                                TurboSx_slow(from_address,ln[1],subject,body,pwd,smtp_server,smtp_port)
-                                
-                                print("-> Success", end=" : ")
-                                print(smtp_server)
-                                file_write.write(ln[0]+","+ln[1]+","+ln[2]+","+ln[3]+"\n")
-                                file_write_up.write(ln[0]+","+ln[1]+","+ln[2]+","+ln[3]+"\n")
-                            except Exception as e:
-                                
-                                print("-> Fail ", end=" : ")
-                                print(smtp_server, end=" \n ")
-                                print(e)
-                                fcount=fcount+1
-                                
-                                file_write_f.write(ln[0]+","+ln[1]+","+ln[2]+","+ln[3]+","+smtp_server+","+from_address+"\n")
-                                
-                        count=count+1
-                    file_write.close()
-                    file_write_f.close()
-                    file_write_up.close()
-                    file.close()
-                    os.remove(fpath)
-                    if(fcount==0):
-                        os.remove(wpath_f)
-                    
-                else:
-                    print("Invalid File Extension "+i)
-                    os.remove(fpath)
-            ftpTransfer(self.outbound_path,ids+"_"+ndate+".csv")
-        else:
-            print("No Files To Process in inbound")
-    def readBuildFiles(self,path):
-        file= open(path,'r')
-        str=""
-        for line in file:
-            str=str+line.strip()
-        return str
-        
 class Shoot_slow:
     def __init__(self,inbound_path=sys.argv[1]+"\inbound",outbound_path=sys.argv[1]+"\outbound\\"+datetime.now().strftime("%Y%m%d"),builds_path=sys.argv[1]+"\\builds"):        
         self.inbound_path=inbound_path
@@ -502,6 +510,123 @@ class Shoot_slow:
             str=str+line.strip()
         return str
 
+class Shoot_attach:
+    def __init__(self,inbound_path=sys.argv[1]+"\inbound",outbound_path=sys.argv[1]+"\outbound\\"+datetime.now().strftime("%Y%m%d"),builds_path=sys.argv[1]+"\\builds"):        
+        self.inbound_path=inbound_path
+        self.outbound_path=outbound_path
+        self.builds_path=builds_path
+        attachment_path=self.outbound_path+"\\body\\"
+        
+        if not os.path.exists(self.outbound_path):
+            os.makedirs(self.outbound_path)
+            
+        if not os.path.exists(attachment_path):
+            os.makedirs(attachment_path)
+
+        ls=os.listdir(self.inbound_path)
+        t_count=len(ls)
+        ndate=datetime.now().strftime("%Y%m%d")
+        ftpTransfer(self.outbound_path,ids+"_"+ndate+".csv")
+        wpath_up=self.outbound_path+"\\"+ids+"_"+ndate+".csv"
+        if(len(ls)>0):
+            print("Total Files:",len(ls))
+            f_count=1
+            for i in os.listdir(self.inbound_path):
+                now = datetime.now()
+                fpath=self.inbound_path+"\\"+i
+                wpath=self.outbound_path+"\\"+ids+"_"+now.strftime("%H%M%S")+"_"+i
+                wpath_f=self.outbound_path+"\\fail_"+now.strftime("%H%M%S")+"_"+i
+                
+                print("Preparing File "+str(f_count)+" of "+str(t_count))
+                f_count=f_count+1
+                if(i.endswith('.csv')):                    
+
+                    
+                    print("File name: "+fpath)
+                    #print(wpath)
+                    
+                    file= open(fpath,'r')
+                    file_write = open(wpath, 'a+')
+                    file_write_f =  open(wpath_f, 'a+')
+                    file_write_up =  open(wpath_up, 'a+')
+                    count=0
+                    fcount=0
+                    for line in file:
+                        
+                        if(count==0):
+                            ln=line.strip().split(',')
+                            try:
+                            
+                                from_address=ln[0]
+                                pwd=ln[1]
+                                smtp_server=ln[2]
+                                smtp_port=ln[3]
+                                file_write.write(ln[0]+","+ln[1]+","+ln[2]+","+ln[3]+"\n")
+#                                 try:
+#                                     s = smtplib.SMTP(smtp_server, smtp_port)
+#                                     s.connect(smtp_server, smtp_port)
+#                                     s.ehlo()
+#                                     s.starttls()
+#                                     s.ehlo()
+#                                 except Exception as e:
+#                                     print(e)
+#                                     print("Unable to Connect to the smtp server: "+smtp_server+" at port: "+smtp_port)
+                            except Exception as e:
+                                print("Error in First line Configs for File {file}!!!".format(file=fpath))
+                        else:
+                            ln=line.strip().split(',')
+                            print(count, end="::")
+                            print(ln,end="")
+                            subject=self.readBuildFiles(self.builds_path+"\\subject\\"+ln[2]).format(name=ln[0])
+                            #print("Subject :"+subject)
+                            body=self.readBuildFiles(self.builds_path+"\\body\\"+ln[3]).replace("{name}", ln[0]).replace("{email}", ln[1])
+                            #print("Body :"+body)
+                            body_path=self.writeAttachmentFiles(attachment_path,ln[0],ln[1],body)
+                            
+                            try:
+                                TurboSx_Attach(from_address,ln[1],subject,attach_text,pwd,smtp_server,smtp_port,body_path)
+                                
+                                print("-> Success", end=" : ")
+                                print(smtp_server)
+                                file_write.write(ln[0]+","+ln[1]+","+ln[2]+","+ln[3]+"\n")
+                                file_write_up.write(ln[0]+","+ln[1]+","+ln[2]+","+ln[3]+"\n")
+                            except Exception as e:
+                                
+                                print("-> Fail ", end=" : ")
+                                print(smtp_server, end=" \n ")
+                                print(e)
+                                fcount=fcount+1
+                                
+                                file_write_f.write(ln[0]+","+ln[1]+","+ln[2]+","+ln[3]+","+smtp_server+","+from_address+"\n")
+                                
+                        count=count+1
+                    file_write.close()
+                    file_write_f.close()
+                    file_write_up.close()
+                    file.close()
+                    os.remove(fpath)
+                    if(fcount==0):
+                        os.remove(wpath_f)
+                    
+                else:
+                    print("Invalid File Extension "+i)
+                    os.remove(fpath)
+            ftpTransfer(self.outbound_path,ids+"_"+ndate+".csv")
+        else:
+            print("No Files To Process in inbound")
+    def readBuildFiles(self,path):
+        file= open(path,'r')
+        str=""
+        for line in file:
+            str=str+line.strip()
+        return str
+    def writeAttachmentFiles(self,path,name,email,body):
+        file_write =  open(path+email+"_"+name+now.strftime("%H%M%S")+".txt", 'a+')
+        file_write.write(body)
+        file_write.close()
+        return path+email+"_"+name+now.strftime("%H%M%S")+".html"
+        
+
             
 class splitFeeds:
     def __init__(self,feeds_path=sys.argv[1]+"\\feeds",inbound_path=sys.argv[1]+"\\inbound"):
@@ -606,7 +731,8 @@ try:
             print("Press 1 to Split Files (Feeds -> inbound):")
             print("Press 2 to Shoot(Fast) (inbound -> outbound):")
             print("Press 3 to Shoot(Stable/Slow) (inbound -> outbound):")
-            print("Press 4 to logout")
+            print("Press 4 to Shoot(with Attachments) (inbound -> outbound):")
+            print("Press 5 to logout")
             val=input("Enter Option:")
 
             if (val == "1"):
@@ -618,8 +744,12 @@ try:
             elif (val == "3"):
                 print("Option 3")
                 Shoot_slow()
-            elif(val == "4"):
+            elif (val == "4"):
                 print("Option 4")
+                attach_text=input("Enter Mail Text:")
+                Shoot_attach()
+            elif(val == "5"):
+                print("Option 5")
                 #code= requests.get("https://www.rankingsquad.website/smtp_verify.php?NAME={usr}&ID={ids}".format(usr=usr,ids=ids,pwd=pwd)).json()['code']
                 x=False
                 
